a2fea0d2b65414aeca8d2a0b5d868b48760f5cd3
chfle <christian.lehnert.home@gmail.com>
2022-05-30 12:51:55 +0200

    DayPlan controller

---
+++ src/main/java/com/lokcenter/AZN/controller/DayPlanController.java
@@ -0,0 +1,25 @@
+package com.lokcenter.AZN.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+@RequestMapping("/")
+public class DayPlanController {
+
+    @GetMapping
+    String getDayPlan(Model model) {
+        //TODO: Add DayPlan data
+
+        // Page title
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        model.addAttribute("title", formatter.format(new Date()));
+
+        return "dayPlan";
+    }
+}
