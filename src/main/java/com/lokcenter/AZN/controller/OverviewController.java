43430b39067e3ed93330c933ef67c1f5a0575693
chfle <christian.lehnert.home@gmail.com>
2022-06-02 08:41:31 +0200

    basic controller setup

---
+++ src/main/java/com/lokcenter/AZN/controller/OverviewController.java
@@ -0,0 +1,16 @@
+package com.lokcenter.AZN.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+// Calendar Site
+@RequestMapping("/overview")
+public class OverviewController {
+    @GetMapping
+    String getOverviewPage(Model model) {
+        return "overview";
+    }
+}
