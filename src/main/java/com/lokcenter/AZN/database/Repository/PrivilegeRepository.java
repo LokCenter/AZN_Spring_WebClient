bbce51caf45a222a05cb744891930ef2296bd09d
chfle <christian.lehnert.home@gmail.com>
2022-06-02 08:50:17 +0200

    Repository setup

---
+++ src/main/java/com/lokcenter/AZN/database/Repository/PrivilegeRepository.java
@@ -0,0 +1,7 @@
+package com.lokcenter.AZN.database.Repository;
+
+import com.lokcenter.AZN.database.Privilege;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PrivilegeRepository extends CrudRepository<Privilege, Long> {
+}
---
+++ src/main/java/com/lokcenter/AZN/database/Repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.lokcenter.AZN.database.Repository;
+
+import com.lokcenter.AZN.database.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
