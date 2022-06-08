499f7eccbf4529e173ebe0d06d94f2483ed264a3
chfle <christian.lehnert.home@gmail.com>
2022-06-08 11:06:56 +0200

    Get current date and go left and right

---
+++ src/main/resources/static/js/dayPlan.js
@@ -0,0 +1,34 @@
+/**
+ * DayPlan JS
+ * @version 1.02 2022-06-08
+ */
+
+
+
+// get date html
+let dateSwitch = document.getElementById("dateSwitchDate");
+
+// moment format
+
+const format = "dddd, D MMMM, YYYY";
+
+// set current date
+dateSwitch.innerText = moment().format(format);
+
+// Change date based on button
+let leftDaySwitch = document.getElementById("left-dayPlan-switch");
+let rightDaySwitch = document.getElementById("right-dayPlan-switch");
+
+// to keep track where we are
+let currDate = moment();
+
+// onClick handler
+leftDaySwitch.addEventListener("click", (e) => {
+    currDate = currDate.subtract(1, "day");
+    dateSwitch.innerText = currDate.format(format);
+});
+
+rightDaySwitch.addEventListener("click", (e) => {
+    currDate = currDate.add(1, "day");
+    dateSwitch.innerText = currDate.format(format);
+});
