d211d3425b2c1d1d420276f88dab4bd4954ffb50
LeonardSzillat <leonard.szillat@gmail.com>
2022-06-20 13:14:34 +0200

    changes to AdminPanel

--- src/main/java/com/lokcenter/AZN/controller/AdminOverviewController.java
+++ src/main/java/com/lokcenter/AZN/controller/AdminOverviewController.java
@@ -5,18 +5,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * Admin DayPlan Controller
- *
- * @version 1.01 2022-06-20
- */
 @Controller
-@RequestMapping("/admin/dayplan")
+@RequestMapping("/admin/overview")
 
-public class AdminDayPlanController {
+public class AdminOverviewController {
     @GetMapping
-    String getAdminDayPlan(Model model) {
-        model.addAttribute("title", "Admin Day Plan");
-        return "AdminDayPlan";
+    String getAdminOverview(Model model) {
+        model.addAttribute("title", "Admin Overview");
+        return "adminOverview";
     }
 }
