1a9d7c7260d98332fbd57574697866decc9612f1
LeonardSzillat <leonard.szillat@gmail.com>
2022-06-23 08:35:38 +0200

    display users for testing

--- src/main/java/com/lokcenter/AZN/controller/AdminController.java
+++ src/main/java/com/lokcenter/AZN/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.lokcenter.AZN.controller;
 
+import com.lokcenter.AZN.database.Repository.UserRepository;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+    private final UserRepository userRepository;
+
+    public AdminController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     /**
      * Return Admin Panel for administration
      *
@@ -22,7 +29,9 @@ public class AdminController {
      */
     @GetMapping
     String getAdminPanel(Model model) {
+        var userdata= userRepository.findAll();
         model.addAttribute("title", "Admin Panel");
+        model.addAttribute("users", userdata);
         return "adminPanel";
     }
 }
