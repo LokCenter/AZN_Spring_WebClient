b53e6bc0500d24fcb53543be9c82ffd574509e06
Leirk <lichiuserik@gmail.com>
2022-09-30 15:02:54 +0200

    Added modal for editing user data

--- src/main/resources/static/js/AdminPanel.js
+++ src/main/resources/static/js/AdminPanel.js
@@ -1,11 +1,25 @@
+// Variable for the previous year overview modal
 const prevYearButtons = document.getElementsByClassName("prev-year-btn");
 const prevYearModal = document.getElementById("prev-year-modal");
 const closePrevYearModal = document.getElementsByClassName("close-prev-year-modal")[0];
+// Variables for the edit modal
+const editButtons = document.getElementsByClassName("edit-btn");
+const editModal = document.getElementById("edit-modal");
+const closeEditModal = document.getElementsByClassName("close-edit-modal")[0];
+const saveButton = document.getElementById("save-user-data-button");
 
 function redirect(id) {
     console.log(id);
 }
 
+/**
+ * Adds a user to the admin panel table.
+ * @param name
+ * @param overtime
+ * @param sick
+ * @param glaz
+ * @param vacation
+ */
 function addUserToTable(name, overtime, sick, glaz, vacation) {
     const APTable = document.getElementById("APTable");
     const newRow = APTable.insertRow();
@@ -25,37 +39,93 @@ function addUserToTable(name, overtime, sick, glaz, vacation) {
     let edit = newRow.insertCell();
     requests.innerHTML = "<a href=\"#\">0</a>";
     prevYear.innerHTML = "<button class=\"prev-year-btn\">Anzeigen</button>";
-    edit.innerHTML = "<button>Bearbeiten</button>";
+    edit.innerHTML = "<button class=\"edit-btn\">Bearbeiten</button>";
 
     /**
-     * Adds a click event to the "Anzeigen" button to display the modal showing an overview over the previous years.
+     * Adds a click event to the "Anzeigen" button to display the modal showing an overview of the previous years.
      */
     prevYear.children[0].onclick = () => {
         prevYearModal.style.display = "block";
+        disableMainWindowScrolling();
     };
+
+    /**
+     * Adds a click event to the "Bearbeiten" button to display the modal allowing for changes to be made.
+     */
+    edit.children[0].onclick = () => {
+        editModal.style.display = "block";
+        disableMainWindowScrolling();
+    }
 }
 
 addUserToTable("Bärbel Holland", "1h 18min Guthaben", 2, 1, 2);
 addUserToTable("Richard Alexander Marktdorf", "0h 39min Schuld", 9, 0, 0);
 
 
-// To be removed when deleting hard-coded placeholders
+// Adds event to each "Anzeigen" button which is hard-coded in the HTML file.
+// Can be removed once all table entries are added via JS only.
 for (let button of prevYearButtons) {
     button.addEventListener("click", () => {
         prevYearModal.style.display = "block";
+        disableMainWindowScrolling();
     });
 }
 
+for (let button of editButtons) {
+    button.addEventListener("click", () => {
+        editModal.style.display = "block";
+        disableMainWindowScrolling();
+    });
+}
+/**
+ * Closes prev year modal when clicking the "x".
+ */
 closePrevYearModal.addEventListener("click", () => {
     prevYearModal.style.display = "none";
+    enableMainWindowScrolling();
 });
 
+/**
+ * Closes the prev year modal when clicking outside of it.
+ */
 window.addEventListener("click", (event) => {
     if (event.target === prevYearModal) {
         prevYearModal.style.display = "none";
+        enableMainWindowScrolling();
     }
 });
 
+/**
+ * Closes the edit modal when clicking the "x".
+ */
+closeEditModal.addEventListener("click", () => {
+    editModal.style.display = "none";
+    enableMainWindowScrolling();
+})
+
+/**
+ * Closes the edit modal when clicking outside of it.
+ */
+window.addEventListener("click", (event) => {
+    if (event.target === editModal) {
+        editModal.style.display = "none";
+        enableMainWindowScrolling();
+    }
+})
+
+function disableMainWindowScrolling() {
+    document.body.style.overflow = "hidden";
+    document.body.style.height = "100%";
+}
+
+function enableMainWindowScrolling() {
+    document.body.style.overflow = "auto";
+    document.body.style.height = "auto";
+}
+
+saveButton.addEventListener("click", () => {
+    // Save data
+})
 
 /**
  * Adds data to the year overview table in the modal.
@@ -85,3 +155,4 @@ function addYear(year, work, sick, vacation, glaz, overtime) {
 
 addYear(2021, 365, 0, 30, 0, "00h 00min Guthaben");
 addYear(2022, 365, 0, 30, 0, "00h 00min Guthaben");
+
