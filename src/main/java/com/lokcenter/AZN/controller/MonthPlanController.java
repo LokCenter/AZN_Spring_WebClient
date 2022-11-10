54f5a32e23dd485b7530678391307d555340cb0f
chfle <christian.lehnert.home@gmail.com>
2022-11-10 11:08:22 +0100

    show data year overview admin

---
+++ src/main/java/com/lokcenter/AZN/controller/MonthPlanController.java
@@ -0,0 +1,17 @@
+package com.lokcenter.AZN.controller;
+
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/monthplan")
+public class MonthPlanController {
+    @GetMapping()
+    String getMonthPlan(Model model,
+                        @RegisteredOAuth2AuthorizedClient("userwebapp")
+                        OAuth2AuthorizedClient authorizedClient) {
+        return "monthPlan";
+    }
+}
