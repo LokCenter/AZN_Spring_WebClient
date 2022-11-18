8cfdb0ba8ba3716cb4d649747f7ba1e84125d5c3
chfle <christian.lehnert.home@gmail.com>
2022-11-18 10:43:16 +0100

    remove useless code

--- src/main/resources/static/js/overviewAdmin.js
+++ src/main/resources/static/js/overviewAdmin.js
@@ -145,55 +145,6 @@ rightDaySwitch.addEventListener("click", (e) => {
     });
 });
 
-/**
- * gets the startDateValue of the current visible month and assigns it the correct label with full month name and year
- */
-function updateTimeDisplay() {
-    let timeDisplay = dp.startDate.value;
-    let year = timeDisplay.slice(0, 4);
-    let month = timeDisplay.slice(5, 7);
-    switch (month) {
-        case "01":
-            month = "Januar";
-            break;
-        case "02":
-            month = "Februar";
-            break;
-        case "03":
-            month = "März";
-            break;
-        case "04":
-            month = "April";
-            break;
-        case "05":
-            month = "Mai";
-            break;
-        case "06":
-            month = "Juni";
-            break;
-        case "07":
-            month = "Juli";
-            break;
-        case "08":
-            month = "August";
-            break;
-        case "09":
-            month = "September";
-            break;
-        case "10":
-            month = "Oktober";
-            break;
-        case "11":
-            month = "November";
-            break;
-        case "12":
-            month = "Dezember";
-            break;
-    }
-    let finalDisplay = month + " " + year;
-    document.getElementById("dateSwitchDate").innerText = finalDisplay;
-}
-
 updateTimeDisplay()
 
 // Set color for legend
--- src/main/resources/static/js/overviewUser.js
+++ src/main/resources/static/js/overviewUser.js
@@ -95,54 +95,6 @@ if (!window.location.href.includes("?")) {
     getDaysAsQuery();
 }
 
-/**
- * gets the startDateValue of the current visible month and assigns it the correct label with full month name and year
- */
-function updateTimeDisplay() {
-    let timeDisplay = dp.startDate.value;
-    let year = timeDisplay.slice(0, 4);
-    let month = timeDisplay.slice(5, 7);
-    switch (month) {
-        case "01":
-            month = "Januar";
-            break;
-        case "02":
-            month = "Februar";
-            break;
-        case "03":
-            month = "März";
-            break;
-        case "04":
-            month = "April";
-            break;
-        case "05":
-            month = "Mai";
-            break;
-        case "06":
-            month = "Juni";
-            break;
-        case "07":
-            month = "Juli";
-            break;
-        case "08":
-            month = "August";
-            break;
-        case "09":
-            month = "September";
-            break;
-        case "10":
-            month = "Oktober";
-            break;
-        case "11":
-            month = "November";
-            break;
-        case "12":
-            month = "Dezember";
-            break;
-    }
-    document.getElementById("dateSwitchDate").innerText = month + " " + year;
-}
-
 updateTimeDisplay()
 
 // Set color for legend
