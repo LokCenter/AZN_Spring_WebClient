96df5c35e316d4b272cde13de4320e411178e595
Leirk <lichiuserik@gmail.com>
2022-11-23 10:59:40 +0100

    Request list updates on accept/deny.

--- src/main/resources/static/js/AdminPanel.js
+++ src/main/resources/static/js/AdminPanel.js
@@ -172,16 +172,18 @@ const putRequestsChange = (startDate, endDate, userId, path, res_callback) => {
 }
 
 
-const acceptRequest = (startDate, endDate, userId, path) => {
+const acceptRequest = (startDate, endDate, userId, path, elem) => {
     putRequestsChange(startDate, endDate, userId, path,() => {
 
     });
+    elem.remove();
 }
 
-const deleteRequest = (startDate, endDate, userId, path) => {
+const deleteRequest = (startDate, endDate, userId, path, elem) => {
     putRequestsChange(startDate, endDate, userId, path,() => {
 
     });
+    elem.remove();
 }
 
 /**
@@ -203,14 +205,15 @@ function setRequestModalContent(numOfRequests, eventType, startDate, endDate, us
         requestOverview.style.display = "grid";
         for (let i = 0; i < numOfRequests; i++) {
             requestOverview.innerHTML += `
+                <div class="event-container">
                 <div class="event-type"><p>${eventType[i]}</p></div>
                 <div><p>${startDate[i]}</p></div>
                 <div><p>&ndash;</p></div>
                 <div><p>${endDate[i]}</p></div>
                 <div><svg 
-                onclick="acceptRequest('${startDate[i]}', '${endDate[i]}', ${userid}, 'admin/requests/accept')" class="svg-accept" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg></div>
-                <div><svg onclick="deleteRequest('${startDate[i]}', '${endDate[i]}', ${userid}, 'admin/requests/delete')" class="svg-deny" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg></div>
-                `
+                onclick="acceptRequest('${startDate[i]}', '${endDate[i]}', ${userid}, 'admin/requests/accept', this.parentNode.parentNode)" class="svg-accept" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg></div>
+                <div><svg onclick="deleteRequest('${startDate[i]}', '${endDate[i]}', ${userid}, 'admin/requests/delete', this.parentNode.parentNode)" class="svg-deny" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg></div>
+                </div>`
         }
     }
 }
