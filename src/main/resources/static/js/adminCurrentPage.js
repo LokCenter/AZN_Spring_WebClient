017879e0538ed18f8f0adb6197ff6320c58c652e
Leirk <lichiuserik@gmail.com>
2023-02-21 08:51:12 +0100

    Removed day plan form admin nav bar and adjusted adminCurrentPage.js accordingly

--- src/main/resources/static/js/adminCurrentPage.js
+++ src/main/resources/static/js/adminCurrentPage.js
@@ -2,18 +2,14 @@ const nav = document.getElementsByClassName("nav-main")[0];
 
 // Differentiates the currently viewed page from the other ones.
 // Color needs to be set manually because somehow it doesn't want to accept that property from the CSS file.
-if (location.href.includes("dayplan")) {
+if (location.href.includes("monthplan")) {
     setNavItemsInactive();
     nav.children[0].className = "nav-active";
     nav.children[0].style.color = "var(--clr-accent)"
-} else if (location.href.includes("monthplan")) {
+} else if (location.href.includes("overview")) {
     setNavItemsInactive();
     nav.children[1].className = "nav-active";
     nav.children[1].style.color = "var(--clr-accent)"
-} else if (location.href.includes("overview")) {
-    setNavItemsInactive();
-    nav.children[2].className = "nav-active";
-    nav.children[2].style.color = "var(--clr-accent)"
 }
 // Don't need to check for admin panel since it doesn't have this nav bar
 
