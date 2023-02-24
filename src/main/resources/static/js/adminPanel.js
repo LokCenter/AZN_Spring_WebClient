dd450124ba96d9b1b9eb586b1c4499b786c7a5b3
Leirk <lichiuserik@gmail.com>
2023-02-24 11:28:32 +0100

    Replaced "delete" svg in default values modal with unicode character

--- src/main/resources/static/js/adminPanel.js
+++ src/main/resources/static/js/adminPanel.js
@@ -108,7 +108,7 @@ standardValuesButton.addEventListener("click", () => {
                     newDefaultRow.insertCell().innerText = response.data[data].defaultEndTime;
                     newDefaultRow.insertCell().innerText = response.data[data].defaultPause;
                     newDefaultRow.insertCell().innerText = response.data[data].defaultVacationDays;
-                    newDefaultRow.insertCell().innerHTML = "<div class='svg-delete-container' onclick='deleteDefault(this.parentNode.parentNode)'><svg class=\"svg-delete\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 320 512\"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d=\"M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z\"/></svg></div>";
+                    newDefaultRow.insertCell().innerHTML = "<div class='delete-container' onclick='deleteDefault(this.parentNode.parentNode)'>&#x2716;</div>";
                 }
             }
 
@@ -128,6 +128,7 @@ standardValuesButton.addEventListener("click", () => {
 /**
  * Delete default value row
  */
+
 function deleteDefault(row) {
     let deleteDate = row.children[0].innerText;
     // Get CSRF token
@@ -182,7 +183,7 @@ function addDefault() {
                 newDefaultRow.insertCell().innerText = defaultEndTime.value;
                 newDefaultRow.insertCell().innerText = defaultPause.value;
                 newDefaultRow.insertCell().innerText = defaultVacation.value;
-                newDefaultRow.insertCell().innerHTML = "<div class='svg-delete-container' onclick='deleteDefault(this.parentNode.parentNode)'><svg class=\"svg-delete\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 320 512\"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d=\"M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z\"/></svg></div>";
+                newDefaultRow.insertCell().innerHTML = "<div class='delete-container' onclick='deleteDefault(this.parentNode.parentNode)'>&#x2716;</div>";
                 defaultModalMessage.innerText = "";
                 defaultModalMessage.style.display = "none";
             }
