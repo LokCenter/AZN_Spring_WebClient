b83521406ece3946b6783980d0c8bd7c9f79703b
PaulKerschbaumer <122256592+PaulKerschbaumer@users.noreply.github.com>
2023-12-20 09:45:35 +0100

    Additional fields in admin panel (#79)
    * set up dev config
    
    * Added additional vacation fields to admin panel
    
    * Added approved and elapsed vacation to admin panel
    
    * Restored production config

--- src/main/resources/static/js/helper/yearOverviewTable.js
+++ src/main/resources/static/js/helper/yearOverviewTable.js
@@ -4,24 +4,30 @@
  * @param year
  * @param work
  * @param sick
- * @param vacation
+ * @param availableVacation
+ * @param approvedVacation
+ * @param elapsedVacation
  * @param glaz
  * @param overtime
  * @param tableBodyName
  */
-function addYear(year, work, sick, vacation, glaz, overtime, tableBodyName) {
+function addYear(year, work, sick, availableVacation,  approvedVacation, elapsedVacation, glaz, overtime, tableBodyName) {
     const yearOverviewTableBody = document.getElementById(tableBodyName).getElementsByTagName("tbody")[0];
     let newRow = yearOverviewTableBody.insertRow();
     let newYear = newRow.insertCell();
     let newWork = newRow.insertCell();
     let newSick = newRow.insertCell();
-    let newVacation = newRow.insertCell();
+    let newAvailableVacation = newRow.insertCell();
+    let newApprovedVacation = newRow.insertCell();
+    let newElapsedVacation = newRow.insertCell();
     let newGLAZ = newRow.insertCell();
     let newOvertime = newRow.insertCell();
     newYear.innerText = year;
     newWork.innerText = work;
     newSick.innerText = sick;
-    newVacation.innerText = vacation;
+    newAvailableVacation.innerText = availableVacation;
+    newApprovedVacation.innerText = approvedVacation;
+    newElapsedVacation.innerText = elapsedVacation;
     newGLAZ.innerText = glaz;
     newOvertime.innerText = overtime;
 }
